ba7b57a7797cdef5095c9e0915c2b184061b4aae
alaa281997 <alaabbas97@gmail.com>
2021-07-11 19:17:22 +0300

    add lab11

---
+++ README.md
@@ -0,0 +1,2 @@
+# Spring for Full-Stack Web Apps
+I use the Spring Initializr to create a new application with artifact songr with Web and Thymeleaf (and optionally Devtools) dependencies. Download the zip file and extract the directory it contains. 
---
+++ src/main/java/com/example/demo/Album.java
@@ -0,0 +1,59 @@
+package com.example.demo;
+
+public class Album {
+    private String title;
+    private String artist;
+    private double songCount;
+    private double length;
+    private String imageUrl;
+
+
+    public Album(String title, String artist, double songCount, double length, String imageUrl) {
+        this.title = title;
+        this.artist = artist;
+        this.songCount = songCount;
+        this.length = length;
+        this.imageUrl = imageUrl;
+    }
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public double getSongCount(double v) {
+        return songCount;
+    }
+
+    public void setSongCount(double songCount) {
+        this.songCount = songCount;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
